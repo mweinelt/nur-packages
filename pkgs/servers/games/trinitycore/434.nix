2b36a9aa51f500642e9d45fc1c98ea0c1395ea4d
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-30 04:17:51 +0100

    trinitycore: reduce to supported versions 335, 434 and 837

--- pkgs/servers/games/trinitycore/434.nix
+++ pkgs/servers/games/trinitycore/434.nix
@@ -1,27 +1,7 @@
-{ llvmPackages_11, lib, fetchFromGitHub, cmake, libmysqlclient, git, boost, readline, bzip2 }:
+{ callPackage, ... } @ args:
 
-llvmPackages_11.stdenv.mkDerivation rec {
-  pname = "TrinityCore";
+callPackage ./generic.nix (args // {
+  owner = "The-Cataclysm-Preservation-Project";
   version = "TDB434.20091";
-
-  src = fetchFromGitHub {
-    owner = "The-Cataclysm-Preservation-Project";
-    repo = pname;
-    rev = version;
-    sha256 = "065ihvcf8nil2sgnfrhi9fnlz0m2ni4fl532y21jxjyyzazih4r5";
-  };
-
-  nativeBuildInputs = [ cmake git ];
-  buildInputs = [ libmysqlclient boost readline bzip2 ];
-
-  cmakeFlags = [
-    "-DMYSQL_HOME=${libmysqlclient}/lib/mysql"
-    "-DMYSQL_INCLUDE_DIR=${libmysqlclient.dev}/include/mysql"
-  ];
-
-  meta = with lib; {
-    description = "TrinityCore Open Source MMO Framework";
-    homepage = "https://www.trinitycore.org";
-    license = licenses.gpl2;
-  };
-}
+  sha256 = "065ihvcf8nil2sgnfrhi9fnlz0m2ni4fl532y21jxjyyzazih4r5";
+})
